cd294e2118656d6592e530f39ff674a12b75ca2c
Renz0011 <renzdelatorre00@gmail.com>
2024-09-26 00:29:32 +0800

    solved kth node from end

--- nodefromend.py
+++ nodefromend.py
@@ -24,26 +24,20 @@ class LinkedList:
   
         
 def find_kth_from_end(ll, k):
+    if k == 1:
+        return ll.tail
     slow = ll.head
     fast = ll.head
-    while slow != None:
-        for _ in range(k):
-            print('Fast', fast.value if fast != None else None)
-            if fast != None: 
+    while fast != None: # loop until tail
+        for _ in range(k): # fast steps are based on k value
+            if fast == None and slow == ll.head: # handle k > linked list length
+                return None # out of bounds
+            if fast != None:
                 fast = fast.next
-                
-        print('> Slow',slow.value if slow != None else None)
-        print('> First Loop?',first_loop)
-        if fast == None:
-            if first_loop:
-                return None
-            else:
-                return slow
-                
+        if fast == None and slow == ll.head: # handle k = linked list length
+            return slow
         slow = slow.next
-        first_loop = False
-        
-    print('Returned', slow)
+    return slow
 
 
 my_linked_list = LinkedList(1)
@@ -53,7 +47,7 @@ my_linked_list.append(4)
 my_linked_list.append(5)
 
 
-k = 2
+k = 3
 result = find_kth_from_end(my_linked_list, k)
 
 print('RESULT:',result.value if result != None else None)  # Output: 4
